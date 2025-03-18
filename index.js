b9dc3bde7639fec80789591d5ca9ffdbc637c402
Abdirahman <abdirahman.salah@student.moringaschool.com>
2025-03-18 15:50:33 +0300

    Add index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// Function expression called divide
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  // Arrow function called square that takes one parameter
+  const square = x => x * x;
+  
+  // Arrow function called add that takes two parameters
+  const add = (a, b) => a + b;
+  
